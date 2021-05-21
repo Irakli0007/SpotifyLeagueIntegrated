22873abff0eb7383fe46641b95a49fc610485bab
Irakli Gvino <iraklig0007@gmail.com>
2021-05-21 01:29:37 -0400

    initial push

---
+++ src/main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron')
+require('electron-reloader')(module)
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      contextIsolation: false
+    }
+  })
+
+  win.loadFile('index.html')
+
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+
